591cf84a24cbaf64f68fc9159aa3d246ee9cc37d
Mr.Passer-by <dr397379567@gmail.com>
2013-11-13 23:56:23 +0800

    add bootstrap

---
+++ protected/views/layouts/footer.php
@@ -0,0 +1,9 @@
+<footer id="footer" class="footer">
+    <nav>
+        <ul>
+            <li>Copyright &copy; <?php echo date('Y'); ?> by My Company.</li>
+            <li>All Rights Reserved.</li>
+            <li><?php echo Yii::powered(); ?></li>
+        </ul>
+    </nav>
+</footer><!-- footer -->
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -4,24 +4,19 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
+  <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+  <?php Yii::app()->clientScript->registerCoreScript('jquery');?>
+  <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" rel="stylesheet">
+  <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/custom.css" rel="stylesheet">
+  <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap.js"></script>
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
 <body>
 
 <div class="container" id="page">
-
+  <?php $this->beginContent('/layouts/header'); ?>
+  <?php $this->endContent();?>
 	<div id="header">
 		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
@@ -47,13 +42,10 @@
 
 	<div class="clear"></div>
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
+
 
 </div><!-- page -->
-
+<?php $this->beginContent('/layouts/footer'); ?>
+<?php $this->endContent();?>
 </body>
 </html>
