e0d3bb5db7c7058d0c848616aaa122fe5866c1f4
Luke Du <lukedu@augmentum.com.cn>
2013-11-14 09:48:20 +0800

    refine php

--- protected/views/layouts/header.php
+++ protected/views/layouts/header.php
@@ -1,4 +1,45 @@
-<header class="nav">
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/testdrive/"><?php echo CHtml::encode(Yii::app()->name); ?></a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="/testdrive/">Home</a></li>
+                <li><a href="/testdrive/index.php/site/page?view=about">About</a></li>
+                <li><a href="/testdrive/index.php/site/contact">Contact</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                <?php if (Yii::app()->user->name == "Guest")
+                    {
+                ?>
+                <li><a href="/testdrive/index.php/site/login">Login</a></li>
+                <?php }
+                    else
+                    {
+                ?>
+                <a class="dropdown-toggle" data-toggle="dropdown" href="">
+                    <?php echo(Yii::app()->user->name) ?><b class="caret"></b>
+                </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="">Profile</a></li>
+                        <li><a href="">Setting</a></li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="/testdrive/index.php/site/logout">Logout</a>
+                        </li>
+                        </ul>
+                <?php } ?>
+                </li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div><!--/.header-->
 
-
-</header>
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -2,8 +2,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="en" />
   <title><?php echo CHtml::encode($this->pageTitle); ?></title>
   <?php Yii::app()->clientScript->registerCoreScript('jquery');?>
   <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" rel="stylesheet">
@@ -15,34 +15,17 @@
 <body>
 
 <div class="container" id="page">
-  <?php $this->beginContent('/layouts/header'); ?>
-  <?php $this->endContent();?>
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+    <?php $this->beginContent('/layouts/header'); ?>
+    <?php $this->endContent();?>
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+    <?php if(isset($this->breadcrumbs)):?>
+    <?php
+        $this->widget('zii.widgets.CBreadcrumbs', array(
+        'links'=>$this->breadcrumbs,));
+    ?><!-- breadcrumbs -->
+    <?php endif?>
 
-	<?php echo $content; ?>
-
-	<div class="clear"></div>
-
-
+    <?php echo $content; ?>
 
 </div><!-- page -->
 <?php $this->beginContent('/layouts/footer'); ?>
