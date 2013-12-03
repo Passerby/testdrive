06a971f5899379c183bc55ac271e14bf7b9c64c8
luke.du <lukedu@augmentum.com.cn>
2013-12-03 09:28:21 +0800

    Refine header doc

--- protected/views/site/signup.php
+++ protected/views/site/signup.php
@@ -1,7 +1,14 @@
 <?php
-/* @var $this SiteController */
-/* @var $model SignupForm */
-/* @var $form CActiveForm */
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * haahaha
+ *
+ * @var $this SiteController
+ * @var $model SignupForm
+ * @var $form CActiveForm
+ */
 $this->breadcrumbs=array(
     'Sign up',
 );
@@ -9,47 +16,61 @@ $this->breadcrumbs=array(
 <h1>Sign up</h1>
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-    'id'=>'signup-form',
-    'htmlOptions'=>array(
-        'class'=>'form-signup'
-    ),
-    // Please note: When you enable ajax validation, make sure the corresponding
-    // controller action is handling ajax validation correctly.
-    // See class documentation of CActiveForm for details on this,
-    // you need to use the performAjaxValidation()-method described there.
-    'enableAjaxValidation'=>true,
-    'clientOptions'=>array(
-        'validateOnSubmit'=>true,
-    ),
-)); ?>
+<?php
+$form=$this->beginWidget(
+    'CActiveForm', array(
+        'id'=>'signup-form',
+        'htmlOptions'=>array(
+            'class'=>'form-signup'
+        ),
+        // Please note: When you enable ajax validation, make sure the corresponding
+        // controller action is handling ajax validation correctly.
+        // See class documentation of CActiveForm for details on this,
+        // you need to use the performAjaxValidation()-method described there.
+        'enableAjaxValidation'=>true,
+        'clientOptions'=>array(
+            'validateOnSubmit'=>true,
+        ),
+    )
+); ?>
 
     <?php echo $form->errorSummary($model); ?>
 
     <div class="row">
-        <?php echo $form->labelEx($model,'email'); ?>
-        <?php echo $form->textField($model,'email',array(
-            'class'=>'form-control',
-            'placeholder'=>'Email',
-            'required'=>'',
-            'autofocus'=>'',
-        )); ?>
-        <?php echo $form->error($model,'email'); ?>
+        <?php echo $form->labelEx($model, 'email'); ?>
+        <?php
+        echo $form -> textField(
+            $model,
+            'email',
+            array(
+                'class'=>'form-control',
+                'placeholder'=>'Email',
+                'required'=>'',
+                'autofocus'=>''
+            )
+        ); ?>
+        <?php echo $form->error($model, 'email'); ?>
     </div>
 
     <div class="row">
-        <?php echo $form->labelEx($model,'username'); ?>
-        <?php echo $form->textField($model,'username',array(
-            'class'=>'form-control',
-            'placeholder'=>'Username',
-            'required'=>'',
-        )); ?>
-        <?php echo $form->error($model,'username'); ?>
+        <?php echo $form->labelEx($model, 'username'); ?>
+        <?php
+        echo $form->textField(
+            $model,
+            'username',
+            array(
+                'class'=>'form-control',
+                'placeholder'=>'Username',
+                'required'=>'',
+            )
+        );
+        ?>
+        <?php echo $form->error($model, 'username'); ?>
     </div>
 
     <div class="row">
-        <?php echo $form->labelEx($model,'password'); ?>
-        <?php echo $form->passwordField($model,'password',array(
+        <?php echo $form->labelEx($model, 'password'); ?>
+        <?php echo $form->passwordField($model, 'password',array(
             'class'=>'form-control',
             'placeholder'=>'Password',
             'required'=>'',
